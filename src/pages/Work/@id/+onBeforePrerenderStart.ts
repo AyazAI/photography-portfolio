171afd76a1811ca0277e2acbc267f60de0c53686
AyazAI <ayaz.uiit3@gmail.com>
2025-10-14 22:42:33 +0500

    Added instructions

--- src/pages/Work/@id/+onBeforePrerenderStart.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from '../../../supabaseClient';
-
-async function onBeforePrerenderStart() {
-  const { data } = await supabase.from('work').select('id');
-  const urls = data ? data.map(item => `/work/${item.id}`) : [];
-  return urls;
-}
-
-export { onBeforePrerenderStart };
--- src/pages/photography/@id/+onBeforePrerenderStart.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from '../../../supabaseClient';
-
-async function onBeforePrerenderStart() {
-  const { data } = await supabase.from('photography').select('id');
-  const urls = data ? data.map(item => `/photography/${item.id}`) : [];
-  return urls;
-}
-
-export { onBeforePrerenderStart };
